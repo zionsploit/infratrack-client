311d904a0a0aa60e4c5e8025607ea484c986f323
John Mark Banisilon <zionsploit@gmail.com>
2025-02-23 22:50:25 +0800

    feat(Development): :label: Add Types for ResponseProjectStatus

--- src/ServerTypes/Project.ts
+++ src/ServerTypes/Project.ts
@@ -4,6 +4,8 @@ export type Project = { project_funded: string, project_code: string, project_st
 
 export type ProjectDetails = { contractor: number, contract_cost: number, start_date: string, end_date: string, day_extension: number, };
 
+export type ResponseProjectStatus = { id: number, status: string, created_at: string, updated_at: string, };
+
 export type ReturnProject = { id: number, project_funded: string, project_year: number, project_code: string, project_status_id: number, project_barangay_id: number, appropriation: number, approved_budget_contract: number, contract_detail_id: number, project_type_id: number, project_category_id: number, project_source_of_fund_id: number, project_mode_of_implementation_id: number, project_sustainable_developement_id: number, project_sector_id: number, project_taker_id: number, accomplished: number, remarks: string, prepared_by: string, created_at: string, updated_at: string, };
 
 export type UpdateProjectById = { id: number, project_funded: string, project_code: string, project_status_id: number, project_barangay_id: number, appropriation: number, approved_budget_contract: number, contract_detail_id: number, project_type_id: number, project_category_id: number, project_source_of_fund_id: number, project_mode_of_implementation_id: number, project_sustainable_developement_id: number, project_sector_id: number, project_taker_id: number, accomplished: number, remarks: string, prepared_by: string, };
